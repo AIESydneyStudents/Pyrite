2a54b307fa04785bef8915620b51877a21c31560
Justin <justinkatic4@gmail.com>
2020-09-27 22:01:28 +1000

    added platform movement script Justin Sandbox
    script allows you to add nodes for a platform to follow in order list size is adjustable got player sticking to platforms properly also now making it become a child of the platform while on the platform.

---
+++ project/Assets/SandBox/JustinSandbox/MoveBetweenNodes.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetweenNodes : MonoBehaviour
+{
+    public Transform[] PlatformPoints;
+    public float speed;
+    public Transform startPos;
+    Vector3 nextPos;
+    private float numberOfPoints;
+    private int currentPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+
+        numberOfPoints = PlatformPoints.Length;
+        currentPoint = 0;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+        if (transform.position == PlatformPoints[currentPoint].position)
+        {
+
+            if (currentPoint < numberOfPoints - 1)
+            {
+                nextPos = PlatformPoints[currentPoint + 1].position;
+                currentPoint += 1;
+            }
+            else
+            {
+                nextPos = startPos.position;
+                currentPoint = 0;
+            }
+           
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < PlatformPoints.Length -1; i++)
+        {
+            Gizmos.DrawLine(PlatformPoints[i].position, PlatformPoints[i+1].position);
+        }
+        
+    }
+}
