91788a22db6d9ed6206f4dc3787dfedea2af65db
Igor Doslov <s191719@students.aie.edu.au>
2020-09-30 12:20:11 +1000

    Igor added personal sandbox

---
+++ project/Assets/SandBox/IgorSandBox/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Handles the main menu
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    public string newGameScene;
+
+    public GameObject playFirstButton;
+
+    public GameObject button;
+
+    private void Start()
+    {
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(playFirstButton);
+    }
+
+    private void Update()
+    {
+
+    }
+    public void PlayGame()
+    {
+        FindObjectOfType<SceneLoader>().LoadNextScene();
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ project/Assets/SandBox/IgorSandBox/Scripts/SceneLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Loads the next scene
+/// </summary>
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public List<string> scenes = new List<string>();
+    public int currentScene = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        var sceneIndex = SceneManager.GetSceneByName(scenes[currentScene]).buildIndex;
+        StartCoroutine(LoadScene(sceneIndex));
+        currentScene += 1;
+
+        if (currentScene == scenes.Count)
+            currentScene = 0;
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
