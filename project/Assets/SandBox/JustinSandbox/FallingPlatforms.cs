5d724dce9a8d0c59bb5db01ecef9d325669893d5
Justin <justinkatic4@gmail.com>
2020-10-04 20:34:47 +1100

    added falling patforms

---
+++ project/Assets/SandBox/JustinSandbox/FallingPlatforms.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatforms : MonoBehaviour
+{
+    public float fallDelay = 1f;
+    public float RespawnDelay = 5f;
+    public GameObject player;
+
+    private Rigidbody rb;
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Invoke("DropPlatform", fallDelay);
+            Invoke("RespawnPlatform", fallDelay + RespawnDelay);
+        }
+    }
+
+    private void DropPlatform()
+    {
+        rb.isKinematic = false;
+    }
+    private void RespawnPlatform()
+    {
+        transform.position = startPos;
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+    }
+
+}
