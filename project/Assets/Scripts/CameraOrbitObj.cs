703ccd451f0bbfdd292ad75e5bc671706fdcad43
Justin <justinkatic4@gmail.com>
2020-10-22 16:27:54 +1100

    build day 2

---
+++ project/Assets/Scripts/CameraOrbitObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbitObj : MonoBehaviour
+{
+    public Transform target;
+    public float rotateSpeed;
+
+
+    private void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.right * rotateSpeed * Time.deltaTime);
+    }
+
+}
