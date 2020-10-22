fe8ce5f2ce18345d18965b6f87d9d3257b4f7d2c
Justin <justinkatic4@gmail.com>
2020-10-22 19:58:30 +1100

    added dummy ui for tea tracking

--- project/Assets/Scripts/TeaTracker.cs
+++ project/Assets/Scripts/TeaTracker.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TeaTracker : MonoBehaviour
 {
     public int teaCollected;
     public PlayerData data;
+    public TextMeshProUGUI teaCountTxt;
 
     private void Start()
     {
         Load();
+        teaCountTxt.text = "Tea Collected : " + teaCollected.ToString();
+    }
+    private void Update()
+    {
+        teaCountTxt.text = "Tea Collected : " + teaCollected.ToString();
     }
     void Load()
     {
