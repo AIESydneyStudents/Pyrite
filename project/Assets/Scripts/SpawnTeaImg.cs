4be2e47207629db356369969ce9163f1bfec7de9
Justin <justinkatic4@gmail.com>
2020-10-26 16:23:46 +1100

    created a tea collected animation

---
+++ project/Assets/Scripts/SpawnTeaImg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTeaImg : MonoBehaviour
+{
+    public Transform startPos;
+    public GameObject teaImg;
+    private GameObject teaClone;
+    public GameObject canvas;
+
+    public void SpawnTeaCollectable()
+    {
+        teaClone = Instantiate(teaImg);
+        teaClone.transform.SetParent(canvas.transform, false);
+        teaClone.transform.position = startPos.transform.position;
+        teaClone.transform.rotation = startPos.transform.rotation;
+    }
+    private void Update()
+    {
+        //if (teaImg != null)
+        //{
+        //    Destroy(teaClone, 1);
+        //}
+    }
+
+}
