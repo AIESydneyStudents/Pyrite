22a5103a295d65f0d9394b3fa21d7e1785b1f74a
Justin <justinkatic4@gmail.com>
2020-10-29 19:36:48 +1100

    hooked up marshmellow jump pad

---
+++ project/Assets/Scripts/PlayMarshmellowAnim.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMarshmellowAnim : MonoBehaviour
+{
+    Animator anim;
+    private void Start()
+    {
+       anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayMushroomAnim()
+    {
+        anim.Play("MarshmellowAnim");
+    }
+}
