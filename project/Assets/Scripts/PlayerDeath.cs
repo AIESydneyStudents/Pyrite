646759eef4ff31812a48990d5c6516c602594e95
Justin <justinkatic4@gmail.com>
2020-11-02 17:37:26 +1100

    trying new theme music added moving ledges

--- project/Assets/Scripts/PlayerDeath.cs
+++ project/Assets/Scripts/PlayerDeath.cs
@@ -6,16 +6,23 @@ using UnityEngine.SceneManagement;
 public class PlayerDeath : MonoBehaviour
 {  
     private GameMaster gameMaster;
+    private GameObject player;
     private void Start()
     {
         gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == GameObject.FindGameObjectWithTag("Player"))
         {
             gameMaster.playerLives -= 1;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            player.SetActive(false);
+            Invoke("LoadCurrentScene", 2f);
         }
+    }
+    void LoadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
