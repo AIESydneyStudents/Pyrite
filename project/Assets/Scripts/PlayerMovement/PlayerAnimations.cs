ccd5533d73b44e093fd10c9f1784de363d2e2405
Justin <justinkatic4@gmail.com>
2020-11-06 11:34:37 +1100

    2ND PASS OF ANIMATIONS

--- project/Assets/Scripts/PlayerMovement/PlayerAnimations.cs
+++ project/Assets/Scripts/PlayerMovement/PlayerAnimations.cs
@@ -13,23 +13,21 @@ public class PlayerAnimations : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (player.isRunning == true)
-            anim.Play("Running");
+    //void Update()
+    //{
+    //    if (player.isRunning == true)
+    //        anim.Play("Running");
         
+    //    if (player.isIdle == true)
+    //        anim.Play("Idle");
 
-        if (player.isIdle == true)
-            anim.Play("Idle");
+    //    if (player.isJump == true)
+    //        anim.Play("Jump");
 
-        if (player.isJump == true)
-            anim.Play("Jump");
+    //    if (player.isDoubleJump == true)
+    //        anim.Play("Double Jump");
 
-        if (player.isDoubleJump == true)
-            anim.Play("Double Jump");
-
-        if (player.isGrapple == true)
-            anim.Play("Grapple");
-
-    }
+    //    if (player.isGrapple == true)
+    //        anim.Play("Grapple");
+    //}
 }
