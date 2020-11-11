905bef87f44ff093d55967e4a268bbd2e963381f
Igor Doslov <s191719@students.aie.edu.au>
2020-11-11 22:56:38 +1100

    Testing particle effects in UI

---
+++ project/Assets/SandBox/IgorSandBox/Scripts/ButtonSelected.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSelected : MonoBehaviour, ISelectHandler
+{
+    private GameObject button;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " was selected");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
