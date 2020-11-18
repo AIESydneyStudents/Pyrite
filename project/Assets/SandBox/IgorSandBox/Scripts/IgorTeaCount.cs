1008cbf104de435239df1f3e2af47a883f8d49aa
IgorDoslov <igordoslov@protonmail.com>
2020-11-18 15:32:35 +1100

    win screen WIP

---
+++ project/Assets/SandBox/IgorSandBox/Scripts/IgorTeaCount.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgorTeaCount : MonoBehaviour
+{
+    public int myTeaLeafCount = 0;
+
+    public void IncreaseLeafCount()
+    {
+        myTeaLeafCount += 1;
+    }
+}
---
+++ project/Assets/SandBox/IgorSandBox/Scripts/WinScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinScreen : MonoBehaviour
+{
+    public TextMeshProUGUI teaScoreTxt;
+    //public TeaTracker teaTracker;
+    public IgorTeaCount igorTeaCount;
+    public GameObject[] star;
+
+    private void Start()
+    {
+        igorTeaCount = FindObjectOfType<IgorTeaCount>();
+
+        star[0].SetActive(false);
+        star[1].SetActive(false);
+        star[2].SetActive(false);
+    }
+
+    public void YouWin()
+    {
+        teaScoreTxt.text = "YOU WIN!";
+        if(igorTeaCount.myTeaLeafCount == 1)
+        {
+            star[0].SetActive(true);
+        }
+        else if(igorTeaCount.myTeaLeafCount == 2)
+        {
+            star[0].SetActive(true);
+            star[1].SetActive(true);
+
+        }
+        else if(igorTeaCount.myTeaLeafCount >= 3)
+        {
+            star[0].SetActive(true);
+            star[1].SetActive(true);
+            star[2].SetActive(true);
+
+        }
+    }
+}
