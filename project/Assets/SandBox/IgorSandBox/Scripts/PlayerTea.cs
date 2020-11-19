29523943d50a3075ed8680ebe5f1529f501af127
IgorDoslov <igordoslov@protonmail.com>
2020-11-19 12:57:32 +1100

    Win screen leaf bar fills up gradually

--- project/Assets/SandBox/IgorSandBox/Scripts/PlayerTea.cs
+++ project/Assets/SandBox/IgorSandBox/Scripts/PlayerTea.cs
@@ -5,30 +5,19 @@ using UnityEngine;
 public class PlayerTea : MonoBehaviour
 {
     public TeaTracker teaTracker;
-   // public int maxLeaves = 100;
-   // public int minLeaves = 0;
     public int currentLeaves;
     public TeaLeafBar teaLeafBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        //currentLeaves = minLeaves;
         teaLeafBar.SetMaxLeaves(teaTracker.numberOfTeaLeaves);
-        //teaLeafBar.SetTeaAmount(minLeaves);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //currentLeaves = teaTracker.numberOfTeaLeaves;
         teaLeafBar.SetTeaAmount(teaTracker.teaCollected);
     }
 
-    //public void CollectTeaLeaf(int leaves)
-    //{
-    //    currentLeaves += leaves;
-
-    //    teaLeafBar.SetTeaAmount(currentLeaves);
-    //}
 }
--- project/Assets/SandBox/IgorSandBox/Scripts/WinScreen.cs
+++ project/Assets/SandBox/IgorSandBox/Scripts/WinScreen.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class WinScreen : MonoBehaviour
 {
     public TextMeshProUGUI teaScoreTxt;
     public TeaTracker teaTracker;
-    //public IgorTeaCount igorTeaCount;
     public GameObject[] star;
+    public GameObject winScreen;
+    
 
     public int firstStarLimit;
     public int secondStarLimit;
     public int thirdStarLimit;
-    
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     private void Start()
-    {
-        //igorTeaCount = FindObjectOfType<IgorTeaCount>();
+    { 
+        slider.value = 0;
         teaTracker = FindObjectOfType<TeaTracker>();
 
         star[0].SetActive(false);
@@ -27,8 +32,6 @@ public class WinScreen : MonoBehaviour
 
     public void YouWin()
     {
-       // int leavesLeft = GameObject.FindGameObjectsWithTag("TeaCollectable").Length;
-
         int leavesCollected = teaTracker.teaCollected;
 
         float percentage = (float)leavesCollected / (float)teaTracker.numberOfTeaLeaves * 100f;
@@ -36,20 +39,33 @@ public class WinScreen : MonoBehaviour
         teaScoreTxt.text = "YOU WIN!";
 
 
-        if(percentage >= firstStarLimit && percentage < secondStarLimit)
+        if (percentage >= firstStarLimit && percentage < secondStarLimit)
         {
             star[0].SetActive(true);
         }
-        else if(percentage >= secondStarLimit && percentage < thirdStarLimit)
+        else if (percentage >= secondStarLimit && percentage < thirdStarLimit)
         {
             star[0].SetActive(true);
             star[1].SetActive(true);
         }
-        else if(percentage == 100)
+        else if (percentage == 100)
         {
             star[0].SetActive(true);
             star[1].SetActive(true);
             star[2].SetActive(true);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (winScreen.activeSelf == true)
+        {
+            if (slider.value < teaTracker.teaCollected)
+            {
+                slider.value = slider.value + (teaTracker.teaCollected * Time.deltaTime);
+                fill.color = gradient.Evaluate(slider.normalizedValue);
+            }
         }
     }
 }
